c5824d2c2af5bcf518f4b16cd262bc9bce0a2307
Lamhotjm <lamhotjms@gmail.com>
2020-03-06 22:58:05 -0600

    add impl

--- src/main/java/edu/miu/cs/cs425/lab9/eregistrar/service/StudentRegistrationService.java
+++ src/main/java/edu/miu/cs/cs425/lab9/eregistrar/service/StudentRegistrationService.java
@@ -1,13 +1,14 @@
 package edu.miu.cs.cs425.lab9.eregistrar.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import edu.miu.cs.cs425.lab9.eregistrar.model.StudentRegistration;
 
 public interface StudentRegistrationService {
 	List<StudentRegistration> findAll();
 	StudentRegistration save(StudentRegistration studentRegistration);
-	StudentRegistration findOne(Long id);
+	Optional<StudentRegistration> findOne(Long id);
 	void delete(Long id);
 
 }
--- src/main/java/edu/miu/cs/cs425/lab9/eregistrar/service/impl/StudentRegistrationServiceImpl.java
+++ src/main/java/edu/miu/cs/cs425/lab9/eregistrar/service/impl/StudentRegistrationServiceImpl.java
@@ -1,9 +1,49 @@
 package edu.miu.cs.cs425.lab9.eregistrar.service.impl;
 
-public class StudentRegistrationServiceImpl {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.miu.cs.cs425.lab9.eregistrar.model.StudentRegistration;
+import edu.miu.cs.cs425.lab9.eregistrar.repository.StudentRegistrationRepository;
+import edu.miu.cs.cs425.lab9.eregistrar.service.StudentRegistrationService;
+
+@Service("studentRegistrationService")
+public class StudentRegistrationServiceImpl implements StudentRegistrationService{
+
+	@Autowired
+	StudentRegistrationRepository studentRegistrationRepository;
+	
 	public StudentRegistrationServiceImpl() {
 		// TODO Auto-generated constructor stub
+		
+	}
+
+	@Override
+	public List<StudentRegistration> findAll() {
+		// TODO Auto-generated method stub
+		return studentRegistrationRepository.findAll();
+	}
+
+	@Override
+	public StudentRegistration save(StudentRegistration studentRegistration) {
+		// TODO Auto-generated method stub
+		return studentRegistrationRepository.save(studentRegistration);
+	}
+
+	@Override
+	public Optional<StudentRegistration> findOne(Long id) {
+		// TODO Auto-generated method stub
+		return studentRegistrationRepository.findById(id);
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		studentRegistrationRepository.deleteById(id);
+		
 	}
 
 }
