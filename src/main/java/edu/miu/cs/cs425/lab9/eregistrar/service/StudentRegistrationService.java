bcffd10d5ba611c019235291a988fb011d0b42b7
Lamhotjm <lamhotjms@gmail.com>
2020-03-06 22:51:09 -0600

    add service interface

--- src/main/java/edu/miu/cs/cs425/lab9/eregistrar/service/StudentRegistrationService.java
+++ src/main/java/edu/miu/cs/cs425/lab9/eregistrar/service/StudentRegistrationService.java
@@ -1,5 +1,13 @@
 package edu.miu.cs.cs425.lab9.eregistrar.service;
 
+import java.util.List;
+
+import edu.miu.cs.cs425.lab9.eregistrar.model.StudentRegistration;
+
 public interface StudentRegistrationService {
+	List<StudentRegistration> findAll();
+	StudentRegistration save(StudentRegistration studentRegistration);
+	StudentRegistration findOne(Long id);
+	void delete(Long id);
 
 }
