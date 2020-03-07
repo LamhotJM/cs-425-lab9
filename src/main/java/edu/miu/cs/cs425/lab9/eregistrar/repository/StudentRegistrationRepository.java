f132187bc535f8082a8a1ab0a7bf361655196572
Lamhotjm <lamhotjms@gmail.com>
2020-03-06 22:47:35 -0600

    add repository

--- src/main/java/edu/miu/cs/cs425/lab9/eregistrar/repository/StudentRegistrationRepository.java
+++ src/main/java/edu/miu/cs/cs425/lab9/eregistrar/repository/StudentRegistrationRepository.java
@@ -1,5 +1,11 @@
 package edu.miu.cs.cs425.lab9.eregistrar.repository;
 
-public interface StudentRegistrationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.miu.cs.cs425.lab9.eregistrar.model.StudentRegistration;
+
+@Repository
+public interface StudentRegistrationRepository extends JpaRepository<StudentRegistration,Long> {
 
 }
